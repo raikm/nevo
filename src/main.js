8a8b5acacd3d90e90a247645c63587e65abed042
raikm <raik.mueller@copra-system.de>
2020-06-26 21:56:40 +0200

    aftership global

--- src/main.js
+++ src/main.js
@@ -9,15 +9,19 @@ import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import VueAxios from './plugins/axios'
 import './mixins/helpersMixin'
+import aftership from './plugins/aftership'
+
 
 
 
 Vue.use(VueAxios)
+Vue.use(aftership)
+
 
 Vue.config.productionTip = false
 Vue.http = Vue.prototype.$http = axios
 window.axios = require('axios')
-
+window.aftership = require('aftership')
 
 Vue.use(Buefy)
 
