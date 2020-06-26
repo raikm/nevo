be81070b1d4d0e6d7ffb4db2054b0e8a8c143b91
raikm <raik.mueller@copra-system.de>
2020-06-26 21:56:59 +0200

    TODO

--- src/mixins/helpersMixin.js
+++ src/mixins/helpersMixin.js
@@ -3,6 +3,7 @@ import Vue from "vue";
 Vue.mixin({
   methods: {
     getConfigFile: function() {
+      //TODO: request also the value and return already the value not the config
       var config = require("../../config.json");
       return config;
     },
