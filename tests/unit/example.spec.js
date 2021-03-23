fa84b5a430262d17ed74f7fb9bf9606819a76589
raikm <mail@raik-mueller.com>
2021-03-23 12:48:25 +0100

    Jest

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,9 @@
+// import { shallowMount } from '@vue/test-utils'
+// import App from '@/components/App.vue'
+
+describe('Start App.vue', () => {
+  it('render', () => {
+    console.log("basic test")
+  })
+  
+})
