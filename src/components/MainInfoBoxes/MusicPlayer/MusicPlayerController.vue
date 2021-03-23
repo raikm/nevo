dd1ee88ba065c62acfe144e53363da3ef813c7fe
raikm <mail@raik-mueller.com>
2021-03-23 14:02:31 +0100

    Update MusicPlayerController.vue

--- src/components/MainInfoBoxes/MusicPlayer/MusicPlayerController.vue
+++ src/components/MainInfoBoxes/MusicPlayer/MusicPlayerController.vue
@@ -128,7 +128,7 @@ export default {
   sockets: {},
   mounted() {
     if (
-      typeof this.activeSpeakerState !== "undefined" &&
+      typeof this.activeSpeakerState != null &&
       this.activeSpeakerState.playbackState === "PLAYING"
     ) {
       this.interval = setInterval(() => {
