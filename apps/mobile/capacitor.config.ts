afb77837fbf7348b77d7c0335eeacfc56fda983c
Raik <mail@raik-mueller.com>
2022-02-25 12:41:18 +0100

    fix: pacakges

--- apps/mobile/capacitor.config.ts
+++ apps/mobile/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.nevo.dashboard.app',
+  appId: 'com.nevo.app',
   appName: 'nevo',
   webDir: 'www',
   bundledWebRuntime: false
