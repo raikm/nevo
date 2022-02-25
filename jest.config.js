0e1f62b5c1baf03268712cb7ce9f883767ea4aac
Raik <mail@raik-mueller.com>
2022-02-25 09:24:39 +0100

    cleanup before merge

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  // "collectCoverage": true,
-  // "collectCoverageFrom": ["**/*.{js,vue}", "!**/node_modules/**"]
-}
--- tests/unit/DashboardPage/MusicControl.spec.ts
+++
@@ -1,8 +0,0 @@
-
-describe("public transport component and Hafas API not available", () => {
-
-  test("should render Service offline container", () => {
-    //TODO
-  });
-});
-
