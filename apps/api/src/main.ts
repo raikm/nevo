b50512db2da833ade8d0f6e8c308e8431d582d2c
Raik <mail@raik-mueller.com>
2023-02-14 21:08:42 +0100

    feat: nestjs added and deno removed

---
+++ apps/api/src/main.ts
@@ -0,0 +1,41 @@
+import {
+  ValidationPipe,
+  VersioningType,
+  VERSION_NEUTRAL,
+} from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import { AppModule } from './app/app.module.js';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
+
+  // app.useLogger(logger);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors({
+    origin: '*',
+  });
+  app.use(cookieParser());
+  app.use(helmet());
+
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: VERSION_NEUTRAL,
+  });
+
+  const port = 5001;
+
+  await app.listen(port);
+
+  // logger.log(
+  //   `🚀 API Application is running on: http://localhost:${port}/${globalPrefix}`,
+  // );
+}
+
+bootstrap();
--- apps/api/types/plant.interface.ts
+++
@@ -1,9 +0,0 @@
-// TODO turbo package
-export interface PlantMeasurement {
-  battery: number
-  soilFertility: number
-  soilMoisture: number
-  sunlight: number
-  temperature: number
-  id: number
-}
